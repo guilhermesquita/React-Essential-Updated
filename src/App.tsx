c21ea2b0979fc47a0882e1d9469ce03cf0dc6993
Guilherme Mesquita <guirozmesquita@gmail.com>
2022-05-16 19:32:33 -0400

    testando os componentes e props

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ function App() {
 
   <Tweet text = "Tweet 1"/>
   <Tweet text = "Tweet 2"/>
-  <Tweet />
+  <Tweet num= {2}/>
 
   </>
   )
--- src/components/tweet.tsx
+++ src/components/tweet.tsx
@@ -1,9 +1,15 @@
+//DEFININDO AS PROPS
+
 type TweetProps = {
     text?: string;
+    num?: number;
 }
 
 export function Tweet(props: TweetProps){ //Definindo a props
     return(
-        <p>{props.text}</p>
+        <>
+        <p>{props.text}</p>  
+        <p>{props.num}</p>   
+        </>
     )
 }
